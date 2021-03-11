1c107c70b50126f22e7ac157cbc5e93c2a86ec90
cycneuramus <cycneuramus@local>
2021-03-11 02:00:01 +0100

    20210311_020001

--- bin/dots-cron.sh
+++ bin/dots-cron.sh
@@ -4,5 +4,6 @@
 
 commit_msg=$(date +"%Y%m%d_%H%M%S")
 git --git-dir=$HOME/.dots/ --work-tree=$HOME commit -a -m "$commit_msg"
+git --git-dir=$HOME/.dots/ --work-tree=$HOME push
 
 push "Punktfiler har synkats på $(hostname)"
