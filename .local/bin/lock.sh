5a27e3f7253f7d34cb0bddba734dcc9990b670e8
cycneuramus <cycneuramus@local>
2021-04-06 16:00:42 +0200

    20210406-160042

--- .local/bin/lock.sh
+++ .local/bin/lock.sh
@@ -8,22 +8,22 @@ H='#81a1c1'
 T='#eceff4'
 W='#Bf616a'
 
-i3lock \
---insidevercolor=$C   \
---ringvercolor=$D     \
-\
---insidewrongcolor=$C \
---ringwrongcolor=$W   \
-\
---insidecolor=$C      \
---ringcolor=$D        \
---linecolor=$D        \
---separatorcolor=$D   \
-\
---verifcolor=$T        \
---wrongcolor=$T        \
---layoutcolor=$T      \
---keyhlcolor=$H       \
---bshlcolor=$H        \
-\
---blur 5              \
+i3lock -C -i $BIN/lock.png 	\
+--insidevercolor=$C   	\
+--ringvercolor=$D     	\
+						\
+--insidewrongcolor=$C 	\
+--ringwrongcolor=$W   	\
+						\
+--insidecolor=$C      	\
+--ringcolor=$D        	\
+--linecolor=$D        	\
+--separatorcolor=$D   	\
+						\
+--verifcolor=$T        	\
+--wrongcolor=$T        	\
+--layoutcolor=$T      	\
+--keyhlcolor=$H       	\
+--bshlcolor=$H        	\
+						\
+--blur 5              	\
