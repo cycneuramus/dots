be0e3fec8b1b903c6eb0093839ef41a2c98403e7
cycneuramus <cycneuramus@local>
2021-04-08 18:23:28 +0200

    20210408-182328

--- .local/bin/brightness.sh
+++ .local/bin/brightness.sh
@@ -8,5 +8,15 @@ fi
 
 current=$(xbacklight -get)
 
+if (( $current < 25 )); then
+	icon="display-brightness-off-symbolic"
+elif (( $current >= 25 && $current < 50 )); then
+	icon="display-brightness-low-symbolic"
+elif (( $current >= 50 && $current < 75 )); then
+	icon="display-brightness-medium-symbolic"
+elif (( $current >= 75 )); then
+	icon="display-brightness-high-symbolic"
+fi
+
 msg_id=991049
-dunstify -t 1500 -i display-brightness-symbolic -r "$msg_id" "Ljusstyrka" "${current}%"
+dunstify -t 1500 -i "$icon" -r "$msg_id" "Ljusstyrka" "${current}%"
