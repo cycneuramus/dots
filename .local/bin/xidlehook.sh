f1d5682aa3d84be7b0ba46523f5bd70a6870a4d0
cycneuramus <cycneuramus@local>
2021-04-10 00:45:17 +0200

    20210410-004517

--- .local/bin/xidlehook.sh
+++ .local/bin/xidlehook.sh
@@ -9,8 +9,8 @@ while pgrep -u $UID -x xidlehook >/dev/null; do sleep 1; done
 xidlehook \
   --not-when-audio \
   --timer 300 \
-  	"$HOME/.local/bin/screensaver.sh" \
+  	'$HOME/.local/bin/screensaver.sh' \
 	'if wmctrl -l | grep unimatrix; then wmctrl -lp | awk "/unimatrix/{print \$3}" | xargs kill; fi' \
   --timer 300 \
-    'wmctrl -lp | awk "/unimatrix/{print \$3}" | xargs kill && sleep 1 && bash $HOME/.local/bin/lock.sh && systemctl suspend' \
-	""
+  'if [[ $(acpi) == *Discharging* ]]; then wmctrl -lp | awk "/unimatrix/{print \$3}" | xargs kill && sleep 1 && bash $HOME/.local/bin/lock.sh && systemctl suspend; fi' \
+	''
