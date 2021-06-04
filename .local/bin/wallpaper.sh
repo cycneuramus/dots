87daf7f1ccf3c13e013f476ba42a80fb1e9f60eb
cycneuramus <cycneuramus@local>
2021-06-04 10:16:17 +0200

    20210604_101616

--- .local/bin/wallpaper.sh
+++ .local/bin/wallpaper.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 wallpapers_path=/home/antsva/.local/share/wallpapers/nord
-wallpaper=$(ls .local/share/wallpapers/nord/ | shuf -n 1)
+random_wallpaper=$(ls .local/share/wallpapers/nord/ | shuf -n 1)
 
 if command -v xrandr; then
 	for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-		feh --bg-fill ${wallpapers_path}/${wallpaper}
+		feh --bg-fill ${wallpapers_path}/${random_wallpaper}
 	done
-else
-	feh --bg-fill ${wallpapers_path}/${wallpaper}
 fi
