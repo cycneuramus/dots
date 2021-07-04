cd119ca6e6bf737b4e0e93ff0c2fcabf7245010c
cycneuramus <cycneuramus@local>
2021-07-04 21:07:19 +0200

    20210704-210719

---
+++ .local/bin/easy-arch.sh
@@ -0,0 +1,182 @@
+#!/usr/bin/env -S bash -e
+
+# Cleaning the TTY.
+clear
+
+# Checking the microcode to install.
+CPU=$(grep vendor_id /proc/cpuinfo)
+if [[ $CPU == *"AuthenticAMD"* ]]
+then
+    microcode=amd-ucode
+else
+    microcode=intel-ucode
+fi
+
+# Selecting the target for the installation.
+PS3="Select the disk where Arch Linux is going to be installed: "
+select ENTRY in $(lsblk -dpnoNAME|grep -P "/dev/sd|nvme|vd");
+do
+    DISK=$ENTRY
+    echo "Installing Arch Linux on $DISK."
+    break
+done
+
+# Deleting old partition scheme.
+read -r -p "This will delete the current partition table on $DISK. Do you agree [y/N]? " response
+response=${response,,}
+if [[ "$response" =~ ^(yes|y)$ ]]
+then
+    wipefs -af "$DISK" &>/dev/null
+    sgdisk -Zo "$DISK" &>/dev/null
+else
+    echo "Quitting."
+    exit
+fi
+
+# Creating a new partition scheme.
+echo "Creating new partition scheme on $DISK."
+parted -s "$DISK" \
+    mklabel gpt \
+    mkpart ESP fat32 1MiB 513MiB \
+    set 1 esp on \
+    mkpart Cryptroot 513MiB 100% \
+
+ESP="/dev/disk/by-partlabel/ESP"
+Cryptroot="/dev/disk/by-partlabel/Cryptroot"
+
+# Informing the Kernel of the changes.
+echo "Informing the Kernel about the disk changes."
+partprobe "$DISK"
+
+# Formatting the ESP as FAT32.
+echo "Formatting the EFI Partition as FAT32."
+mkfs.fat -F 32 $ESP &>/dev/null
+
+# Creating a LUKS Container for the root partition.
+echo "Creating LUKS Container for the root partition"
+cryptsetup luksFormat $Cryptroot
+echo "Opening the newly created LUKS Container."
+cryptsetup open $Cryptroot cryptroot
+BTRFS="/dev/mapper/cryptroot"
+
+# Formatting the LUKS Container as BTRFS.
+echo "Formatting the LUKS container as BTRFS."
+mkfs.btrfs $BTRFS &>/dev/null
+mount $BTRFS /mnt
+
+# Creating BTRFS subvolumes.
+echo "Creating BTRFS subvolumes."
+btrfs su cr /mnt/@ &>/dev/null
+btrfs su cr /mnt/@home &>/dev/null
+btrfs su cr /mnt/@snapshots &>/dev/null
+btrfs su cr /mnt/@var_log &>/dev/null
+
+# Mounting the newly created subvolumes.
+umount /mnt
+echo "Mounting the newly created subvolumes."
+mount -o ssd,noatime,space_cache,compress=zstd,subvol=@ $BTRFS /mnt
+mkdir -p /mnt/{home,.snapshots,/var/log,boot}
+mount -o ssd,noatime,space_cache.compress=zstd,autodefrag,discard=async,subvol=@home $BTRFS /mnt/home
+mount -o ssd,noatime,space_cache,compress=zstd,autodefrag,discard=async,subvol=@snapshots $BTRFS /mnt/.snapshots
+mount -o ssd,noatime,space_cache,compress=zstd,autodefrag,discard=async,subvol=@var_log $BTRFS /mnt/var/log
+chattr +C /mnt/var/log
+mount $ESP /mnt/boot/
+
+# Pacstrap (setting up a base sytem onto the new root).
+echo "Installing the base system (it may take a while)."
+pacstrap /mnt base linux $microcode linux-firmware btrfs-progs grub grub-btrfs efibootmgr networkmanager snapper sudo apparmor reflector base-devel i3-wm lightdm lightdm-gtk-greeter xorg-server git gvim kitty xorg-xrdb rsync wget
+
+echo "Enabling NetworkManager."
+systemctl enable NetworkManager --root=/mnt &>/dev/null
+
+# Generating /etc/fstab.
+echo "Generating a new fstab."
+genfstab -U /mnt >> /mnt/etc/fstab
+
+# Setting hostname.
+read -r -p "Please enter the hostname: " hostname
+echo "$hostname" > /mnt/etc/hostname
+
+# Setting up locales.
+echo "sv_SE.UTF-8 UTF-8"  > /mnt/etc/locale.gen
+echo "LANG=sv_SE.UTF-8" > /mnt/etc/locale.conf
+
+# Setting up keyboard layout.
+echo "KEYMAP=sv-latin1" > /mnt/etc/vconsole.conf
+
+# Setting hosts file.
+echo "Setting hosts file."
+cat > /mnt/etc/hosts <<EOF
+127.0.0.1   localhost
+::1         localhost
+127.0.1.1   $hostname.localdomain   $hostname
+EOF
+
+# Configuring /etc/mkinitcpio.conf.
+echo "Configuring /etc/mkinitcpio.conf for LUKS hook."
+sed -i -e 's,modconf block filesystems keyboard,keyboard keymap modconf block encrypt filesystems,g' /mnt/etc/mkinitcpio.conf
+
+# Setting up LUKS2 encryption and apparmor.
+UUID=$(blkid $Cryptroot | cut -f2 -d'"')
+sed -i "s/quiet/quiet cryptdevice=UUID=$UUID:cryptroot root=$BTRFS lsm=lockdown,yama,apparmor,bpf/g" /mnt/etc/default/grub
+
+# Configuring the system.    
+arch-chroot /mnt /bin/bash -e <<EOF
+    
+    # Setting up timezone.
+    ln -sf /usr/share/zoneinfo/$(curl -s http://ip-api.com/line?fields=timezone) /etc/localtime &>/dev/null
+    
+    # Setting up clock.
+    hwclock --systohc
+
+    # Generating locales.
+    echo "Generating locales."
+    locale-gen &>/dev/null
+
+    # Generating a new initramfs.
+    echo "Creating a new initramfs."
+    mkinitcpio -P &>/dev/null
+
+    # Snapper configuration
+    umount /.snapshots
+    rm -r /.snapshots
+    snapper --no-dbus -c root create-config /
+    btrfs subvolume delete /.snapshots &>/dev/null
+    mkdir /.snapshots
+    mount -a
+    chmod 750 /.snapshots
+
+    # Installing GRUB.
+    echo "Installing GRUB on /boot."
+    grub-install --target=x86_64-efi --efi-directory=/boot/ --bootloader-id=GRUB &>/dev/null
+    
+    # Creating grub config file.
+    echo "Creating GRUB config file."
+    grub-mkconfig -o /boot/grub/grub.cfg &>/dev/null
+
+EOF
+
+# Setting root password.
+echo "Setting root password."
+arch-chroot /mnt /bin/passwd
+
+# Enabling LightDM.
+echo "Enabling LightDM."
+systemctl enable lightdm.service --root=/mnt &>/dev/null
+
+# Enabling AppArmor.
+echo "Enabling AppArmor."
+systemctl enable apparmor --root=/mnt &>/dev/null
+
+# Enabling Reflector timer.
+echo "Enabling Reflector."
+systemctl enable reflector.timer --root=/mnt &>/dev/null
+
+# Enabling Snapper automatic snapshots.
+echo "Enabling Snapper and automatic snapshots entries."
+systemctl enable snapper-timeline.timer --root=/mnt &>/dev/null
+systemctl enable snapper-cleanup.timer --root=/mnt &>/dev/null
+systemctl enable grub-btrfs.path --root=/mnt &>/dev/null
+
+echo "Done, you may now wish to reboot (further changes can be done by chrooting into /mnt)."
+exit
